019b293a8635af066729df46e7ee12a455e311b1
mukhriddinuz <109203312+Muhriddinuz@users.noreply.github.com>
2025-08-03 11:14:10 +0500

    chat

--- home/settings.py
+++ home/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
 
     'chat.apps.ChatConfig',
     'rest_framework',  
-    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
 ]
 
